3f3d9c37ed2c6e08732bd11df0a2920c7e6c2b04
aiswaryapradeep2005 <aiswaryapradeep2005@gmail.com>
2026-02-25 21:44:40 +0530

    Add FizzBuzz function implementation
    Implement FizzBuzz function that generates an array of strings for numbers from 1 to n, replacing multiples of 3 and 5 with 'Fizz' and 'Buzz'.

---
+++ Day3/fizzbuzz.c
@@ -0,0 +1,22 @@
+char** fizzBuzz(int n, int* returnSize) {
+    *returnSize=n;
+    char** answer=(char**)malloc(n*sizeof(char*));
+   int i ;
+   for(i=1;i<=n;i++){
+    answer[i-1]=(char*)malloc(10* sizeof(char));
+   
+    if(i%3==0 && i%5==0){
+        strcpy(answer[i-1], "FizzBuzz");
+    }
+    else if(i%3==0){
+          strcpy(answer[i-1], "Fizz");
+    }
+    else if(i%5==0){
+         strcpy(answer[i-1], "Buzz");
+    }
+    else{
+        sprintf(answer[i-1],"%d",i);
+    }
+   }
+   return answer;
+}
